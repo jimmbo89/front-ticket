36024bb5ae52a7c5bb72067d35f9263996c05602
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2026-06-30 16:54:00 -0400

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8001/api/',
+  //baseURL: 'http://localhost:8001/api/',
   //baseURL: 'https://api.busgo.cl/api/',
-  //baseURL: 'https://busgo.api.klint.cl/api/',
+  baseURL: 'https://busgo.api.klint.cl/api/',
   timeout: 10000,
 });
 
